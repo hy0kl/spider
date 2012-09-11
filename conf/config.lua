ede037e912f2c2d6362fde7133e30e3c7b312c70
Jerry Yang <hy0kle@gmail.com>
2012-09-11 17:21:37 +0800

    use lug to config the spider.

---
+++ conf/config.lua
@@ -0,0 +1,14 @@
+gconfig = {
+    log_name = 'log/test.log',
+    log_level= 4,
+    log_size = 102400,
+    do_daemonize = 1,
+    dir_magic_number = 11,
+    url_density = 23
+};
+
+g_module_count = 2;
+g_module_name  = {
+    'ganji',
+    '58'
+};
